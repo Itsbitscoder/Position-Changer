1658a47c922e27bb3155b441a1ab588bf6faf65b
Kunal Jadhav <154059626+Itsbitscoder@users.noreply.github.com>
2024-07-07 20:47:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ noBtn.addEventListener('click', () => {
     noBtn.style.left = i + 'px';
     noBtn.style.top = j + 'px';
 });
-noBtn.addEventListener('onmouseover', () => {
+noBtn.addEventListener('mouseover', () => {
     const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
     const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
     noBtn.style.left = i + 'px';
